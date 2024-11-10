b1ad52adfbf380587cfc3728ed45cb7189f02f27
SandorMore <msaso0725@gmail.com>
2024-11-10 16:16:13 +0100

    ik

--- idk/todolist.py
+++ idk/todolist.py
@@ -1,11 +1,28 @@
 kerdes = str(input("Mit szeretnél hozzáadni a todo listhez? "))
+
 def Beiras():
     f = open("todolist.txt", "w", encoding="utf-8")
-    f.write("A todo lsit tartalma: ")
     f.write("\n")
     f.write(kerdes)
     f.write("\n")
     f.close
 
+def Beolvasas():
+    container = []
+    j = open("todolist.txt", "r", encoding="utf-8")
+    lines = j.readlines()
+    for line in lines:
+        line = line.strip().split("\n")
+        container.append(line)
+
+    def CheckIfExist(lekerdezes = None):
+        if lekerdezes == None:
+            lekerdezes = str(input("Milyen elemet keresel? "))
+        nonlocal container
+        for i in container:
+
 while kerdes.upper() != "exit":
-    j = open("todolist.txt")
+    Beiras()
+    if kerdes == "exit":
+        break
+    for 
